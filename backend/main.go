6242514effa4c9a64a6aab6e72dcaf6cc4041975
aluminick <archuanickie@gmail.com>
2020-09-09 02:56:59 +0800

    get network interfaces

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("chatapp")
+	interfaces, err := net.Interfaces()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(interfaces)
 }
